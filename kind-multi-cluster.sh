1c4992e7893e261125e0873ff916e825bf253229
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2023-06-20 00:34:50 +0000

    Create kind-multi-cluster.sh

---
+++ kind-multi-cluster.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Purpose: Kubernetes Cluster
+# extraPortMappings allow the local host to make requests to the Ingress controller over ports 80/443
+# node-labels only allow the ingress controller to run on a specific node(s) matching the label selector
+# https://kind.sigs.k8s.io/docs/user/ingress/
+
+
+######################################
+# Docker & Docker Compose Installation
+######################################
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm -f get-docker.sh
+
+
+#######################
+# Kubectl Installation
+#######################
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+####################
+# Helm Installation
+####################
+# https://helm.sh/docs/intro/install/
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+cp /usr/local/bin/helm /usr/bin/helm
+rm -f get_helm.sh
+helm version
+
+###################
+# Kind Installation
+###################
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.19.0/kind-linux-amd64
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
+
+# Latest Version
+# https://github.com/kubernetes-sigs/kind
+# curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.9.0/kind-$(uname)-amd64"
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+chmod +x ./kind
+
+mv ./kind /usr/local/bin
+
+#############
+# Kind Config
+#############
+cat << EOF > kind1-config.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+ apiServerAddress: 0.0.0.0
+ apiServerPort: 8443
+EOF
+
+
+#############
+# Kind Config
+#############
+cat << EOF > kind2-config.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+ apiServerAddress: 0.0.0.0
+ apiServerPort: 8444
+EOF
+  
+# kind create --name cloudgeeks cluster --config kind-config.yaml --image kindest/node:v1.21.10
+  
+  # ssh -N -L 8443:0.0.0.0:8443 cloud_user@d8d0041c.mylabserver.com
+  
+ # export KUBECONFIG=".kube/config"
+  
+
+  
+kind create --name cloudgeeks-local-1 cluster --config kind1-config.yaml --image kindest/node:v1.25.9
+kind create --name cloudgeeks-local-2 cluster --config kind2-config.yaml --image kindest/node:v1.25.9
+ 
+export KUBECONFIG=".kube/config"
+ 
+  #End
