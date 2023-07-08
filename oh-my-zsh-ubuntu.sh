ceff4698e47ddeb9995c3f4a1e15ca1bf9b2a254
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2023-07-08 07:45:40 +0500

    Update oh-my-zsh-ubuntu.sh

--- oh-my-zsh-ubuntu.sh
+++ oh-my-zsh-ubuntu.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 # Update the package lists
-sudo apt-get update
+echo "Updating package lists..."
+sudo apt-get update -qq
 
-# Install ZSH and Git (if not installed)
-sudo apt-get install -y zsh git
+# Check if ZSH and Git are already installed, if not, install them
+if ! command -v zsh &> /dev/null; then
+    echo "ZSH not found, installing..."
+    sudo apt-get install -y zsh
+fi
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! command -v git &> /dev/null; then
+    echo "Git not found, installing..."
+    sudo apt-get install -y git
+fi
 
-# Set ZSH as your default shell
-sudo chsh -s $(which zsh) $USER
+# Check if Oh-My-Zsh is already installed, if not, install it
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh-My-Zsh not found, installing..."
+    # Backup existing .zshrc file if it exists
+    [ -f "$HOME/.zshrc" ] && cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Set ZSH as your default shell if it's not already
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Setting ZSH as default shell..."
+    chsh -s "$(which zsh)"
+fi
