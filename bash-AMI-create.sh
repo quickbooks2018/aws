42b90c3c3eebbdd96b3700f28a72a372d3b4447a
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2023-09-01 14:10:47 +0000

    Update bash-AMI-create.sh

--- bash-AMI-create.sh
+++ bash-AMI-create.sh
@@ -194,3 +194,146 @@ sudo systemctl start apache2.service || die "Failed to start apache2.service"
 echo "Script completed successfully."
 
 # End
+
+
+# Version2 Backups
+
+#!/usr/bin/env bash
+
+set -xe
+
+# Validate superuser privileges
+if [ "$(id -u)" -ne 0 ]; then
+    log_this "Error: This script requires superuser privileges."
+    exit 1
+fi
+
+# Function to log messages
+log_this() {
+    echo "$1"
+}
+
+# Function to exit with an error message
+die() {
+  echo "Error: $1"
+  exit 1
+}
+
+# Function to stop services
+stop_service() {
+    local service_name="$1"
+    log_this "Stopping ${service_name}"
+    systemctl stop "$service_name" || {
+        log_this "Error: Failed to stop ${service_name}"
+        exit 1
+    }
+    sleep 2 # Allow time for service to stop
+}
+
+# Function to start services
+start_service() {
+    local service_name="$1"
+    log_this "Starting ${service_name}"
+    systemctl start "$service_name" || {
+        log_this "Error: Failed to start ${service_name}"
+        exit 1
+    }
+}
+
+# AWS Region variable
+REGION='us-east-1'
+
+# Retention period for AMIs
+RETENTION_PERIOD=3
+
+# Step 1: Stop the apache2.service
+stop_service "apache2.service"
+
+# Step 2: Create an AMI of the current EC2 instance
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+[[ -z "$INSTANCE_ID" ]] && die "Failed to get instance ID"
+
+INSTANCE_NAME=$(aws ec2 describe-tags \
+  --region "$REGION" \
+  --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=Name" \
+  --query 'Tags[0].Value' \
+  --output text) || die "Failed to get instance Name"
+
+[[ -z "$INSTANCE_NAME" ]] && die "Instance Name is empty"
+
+AMI_NAME="$INSTANCE_NAME-ami-$(date +'%Y-%m-%d-%H-%M-%S')"
+
+AMI_ID=$(aws ec2 create-image \
+  --region "$REGION" \
+  --instance-id "$INSTANCE_ID" \
+  --name "$AMI_NAME" \
+  --description "An AMI for apache2.service" \
+  --no-reboot \
+  --query 'ImageId' \
+  --output text) || die "Failed to create AMI"
+
+[[ -z "$AMI_ID" ]] && die "AMI ID is empty"
+
+log_this "AMI creation started. AMI ID is $AMI_ID."
+
+aws ec2 create-tags \
+  --region "$REGION" \
+  --resources "$AMI_ID" \
+  --tags "Key=Name,Value=$INSTANCE_NAME" || die "Failed to tag AMI"
+
+# Step 3: Verify AMI creation
+while true; do
+  AMI_STATE=$(aws ec2 describe-images \
+    --region "$REGION" \
+    --image-ids "$AMI_ID" \
+    --query 'Images[0].State' \
+    --output text) || die "Failed to fetch AMI state"
+
+  if [[ "$AMI_STATE" == "available" ]]; then
+    log_this "AMI is available."
+    break
+  elif [[ "$AMI_STATE" == "pending" ]]; then
+    log_this "AMI is still pending. Waiting..."
+    sleep 10
+  else
+    die "AMI is in an unexpected state: $AMI_STATE"
+  fi
+done
+
+# Step 4: Start the apache2.service again
+start_service "apache2.service"
+
+log_this "Script completed successfully."
+
+# Step 5: Remove old backups, retain last $RETENTION_PERIOD
+
+echo "Removing old backups, retaining last $RETENTION_PERIOD..."
+OLD_AMIS=$(aws ec2 describe-images \
+  --region "$REGION" \
+  --owners "self" \
+  --filters "Name=name,Values=$INSTANCE_NAME-ami-*" \
+  --query "Images | sort_by(@, &CreationDate)" \
+  --output json) || die "Failed to list old AMIs"
+
+# Filter out the latest N AMIs, delete the rest
+TOTAL_AMIS=$(echo "$OLD_AMIS" | jq '. | length')
+AMIS_TO_DELETE=$(echo "$OLD_AMIS" | jq ".[0:$(($TOTAL_AMIS - $RETENTION_PERIOD))]")
+
+for OLD_AMI in $(echo "$AMIS_TO_DELETE" | jq -r '.[].ImageId'); do
+  echo "Deregistering old AMI $OLD_AMI..."
+  aws ec2 deregister-image \
+    --region "$REGION" \
+    --image-id "$OLD_AMI" || die "Failed to deregister $OLD_AMI"
+
+  # Get the snapshot ID associated with the AMI
+  SNAPSHOT_ID=$(echo "$AMIS_TO_DELETE" | jq -r --arg OLD_AMI "$OLD_AMI" '.[] | select(.ImageId == $OLD_AMI) | .BlockDeviceMappings[0].Ebs.SnapshotId')
+
+  # Delete the snapshot
+  echo "Deleting snapshot $SNAPSHOT_ID..."
+  aws ec2 delete-snapshot \
+    --region "$REGION" \
+    --snapshot-id "$SNAPSHOT_ID" || die "Failed to delete snapshot $SNAPSHOT_ID"
+done
+
+# End
+
