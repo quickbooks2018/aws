bf865fff9f9260f4f041275addafa37830259ba4
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2024-08-04 22:19:28 +0500

    Update vmware-tools-ubuntu.sh

--- vmware-tools-ubuntu.sh
+++ vmware-tools-ubuntu.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-# Update the system
-sudo apt update && sudo apt upgrade -y
+sudo apt update
+sudo apt install open-vm-tools-desktop
 
-# Create mount directory
-sudo mkdir -p /mnt/cdrom
-
-# Mount VMware Tools ISO
-sudo mount /dev/cdrom /mnt/cdrom
-
-# Copy VMware Tools to home directory
-cp /mnt/cdrom/VMwareTools-*.tar.gz ~/
-
-# Extract VMware Tools
-tar -xzvf ~/VMwareTools-*.tar.gz
-
-# Install VMware Tools
-sudo ~/vmware-tools-distrib/vmware-install.pl -d
-
-# Reboot the system
 sudo reboot
